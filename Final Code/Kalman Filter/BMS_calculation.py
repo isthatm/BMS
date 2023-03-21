479fecd4f69c5810b158e09c383fee22fb3cb037
Minh Le <91708157+isthatm@users.noreply.github.com>
2023-03-21 20:36:49 +0700

    Update BMS_calculation.py

--- Final Code/Kalman Filter/BMS_calculation.py
+++ Final Code/Kalman Filter/BMS_calculation.py
@@ -1,7 +1,6 @@
 import numpy as np
 import serial
 import time 
-import pandas as pd
 
 # ================= Set up ====================
 serialPort = 'COM4'
@@ -157,78 +156,4 @@ while True:
     currentTime = time.time() - startLoop
 print("Out of battery!!!")
 
-# ================= Debug ====================
-# # TEST KF
-# df = pd.read_csv(r"C:\Users\Admin\Documents\USTH\Battery monitoring system\Data\LIPO-1 1st discharge\rawOCV.csv")
-# KF_OCV = df["OCV"]
-# measuredSOC = [100, 99, 98.36, 97.21, 96.94, 95.42, 94.421, 93.414, 92.5239, 91.29814, 90.149]
-# test_current = -1
-# test_interval = 30 #seconds
-# for i in range (1, len(KF_OCV)):
-#     SOC_OCV = OCVtoSOC(KF_OCV[i], 8)
-#     KF_SOC = KF(30, SOC_OCV, test_current)
-#     #for i in range(1, 11):
-#     CCSoC = CC_SOC(test_interval, test_current)
-#     print("Kalman SOC = %.2f" % KF_SOC)
-#     print("CC SOC = %.2f"  % CCSoC)
-#     #print("Kalman gain = ", KF_gain)    
-#     file.write(str(float(SOC_OCV))+ "," + str(float(KF_SOC)) + "," + str(CCSoC) + "\n")
-
-# # TEST matrix
-# matrix1 = np.array([[1], 
-#                    [3]])
-# matrix2 = np.array([[4, 5], [2, 3]]) 
-# matrix3 = np.array([[4, 5]])
-#print(np.matmul(matrix3, np.transpose(matrix2)))
-# ocvSOC = OCVtoSOC(4.0126, 8)
-# print(' %.2f' % ocvSOC)
-#print("sensor value:", getData(READ_SAMPLING_CURRENT))
-
-# for i in range (1,20):
-#     #schedule.run_pending()
-#     if (var_OCV > 2.8):
-#         file.write
-#     csvData = "1" + "," + "2" + "," + "3" + "," + "4" + "\n"
-#     file.write(csvData)
-#     if (i == 10):
-#         file.close()
-#     #time.sleep(1)
-
-# # TEST CSV
-# currentTime = 0
-# for i in range(1,6):
-#     startTime = time.time()
-#     time.sleep(1)
-#     endTime = time.time()
-#     currentTime += endTime - startTime
-# print(currentTime)
-
-# # TEST Task execution simulation
-# currentTime = 0
-
-# prev1 = 0
-# prev2 = 0
-# prev3 = 0
-
-# interval_1 = 0.5
-# interval_2 = 5
-# interval_3 = 10
-# def delaySim():
-#     time.sleep(6)
-#     print("Delayed")
-
-# while True:
-#         startTime = time.time()
-#         if ( (currentTime - prev1) >= interval_1 ):
-#             print("Task 1 %.2f" % currentTime)
-#             prev1 = currentTime
-#         if ( (currentTime - prev2) >= interval_2 ):
-#             print("Task 2 %.2f" % currentTime)
-#             prev2 = currentTime
-#         if ( (currentTime - prev3) >= interval_3 ):
-#             print("Task 3 %.2f" % currentTime)
-#             delaySim()
-#             prev3 = currentTime
-#         endTime = time.time()
-#         currentTime += (endTime - startTime)
 
